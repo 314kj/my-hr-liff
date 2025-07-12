c707e9a80335b02b8fa6899ac5123390706a03a1
314kj <kaewjaojom.pan@gmail.com>
2025-07-12 16:17:30 +0700

    Update profile.script.js

--- profile.script.js
+++ profile.script.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbx6jf_O19PouFJnxhmlcGuCLg2RdTb6SwalfIuyI5aYY72x8g544wKpbgByR3c_wcva/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbzElcYNmEjvbzJJoJuDx4IxDiW_3kowVZVf9vk1ZzRKk17TdDyZk0HdPXuIs9QvJLl5/exec';
 const LIFF_ID = '2007730528-rw7ewjK7';
 
 // --- DOM Elements (แก้ไขใหม่) ---
